bf6505e92ff4612d4fbee4a3d3bacc7975468d7b
raman1999 <ramanjoshi1999@gmail.com>
2020-07-28 14:00:25 +0530

    m

---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,14 @@
+
+(function(){
+ var helloSpeaker ={};
+
+
+
+var speakWord = "Hello";
+
+
+helloSpeaker.speak=function(name) {
+  console.log(speakWord + " " + name);
+}
+window.helloSpeaker=helloSpeaker;
+})();
